fc4eecb1bc5c731580691a13da186f480d82f8e7
Garrett McKenzie <garrett.david.mckenzie@gmail.com>
2025-11-12 13:37:44 -0500

    Hey Noah I just added a thing to the jsonl converter so that you could argue what files you want to convert

--- data/training_data_to_jsonl.py
+++ data/training_data_to_jsonl.py
@@ -2,6 +2,7 @@ import csv
 import json
 from pathlib import Path
 from typing import List, Union
+import sys
 
 def csvs_to_jsonl(
     csv_files: Union[str, List[str]],
@@ -78,17 +79,43 @@ def csvs_to_jsonl(
     print(f"Wrote {len(episodes)} episodes to {output_path}")
 
 # Use the function here:
-csvs_to_jsonl(
-    csv_files=[
-        "data/frobot_training/bias.csv",
-        "data/frobot_training/misinformation.csv",
-        "data/frobot_training/misrepresentation.csv",
-        "data/frobot_training/pass.csv",
-        "data/frobot_training/prodding.csv",
-        "data/frobot_training/toxicity.csv",
-    ],
-    output_path="data/frobot_training/merged_training_data.jsonl",
-    system_instructions="N/A for now"
-)
+if __name__ == "__main__":
 
+    args = sys.argv
+
+    if len(args) == 1:
+        csvs_to_jsonl(
+            csv_files=[
+                "data/frobot_training/bias.csv",
+                "data/frobot_training/misinformation.csv",
+                "data/frobot_training/misrepresentation.csv",
+                "data/frobot_training/pass.csv",
+                "data/frobot_training/prodding.csv",
+                "data/frobot_training/toxicity.csv",
+            ],
+            output_path="data/frobot_training/merged_training_data.jsonl",
+            system_instructions="N/A for now"
+        )
+    else:
+        try:
+            if len(args) != 3:
+                raise Exception("Usage: python training_data_to_jsonl.py f1.csv,f2.csv,f3.csv output.jsonl")
+            else:
+                inputs = args[1].split(",")
+                output = args[2]
+
+                for f in inputs:
+                    if ".csv" not in f:
+                        raise Exception(f"File {f} is not a csv file!")
+                csvs_to_jsonl(inputs , output_path = output,system_instructions="N/A for now")
+
+                if ".jsonl" not in output:
+                    raise Exception(f"File {f} is not a .jsonl output!")
+
+        except Exception as e:
+            print(e)
+            exit
+
+        csv_paths = args[1]
+        output_path = args[2]
 # run python -m data.training_data_to_jsonl
