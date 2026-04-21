0dd3949abddd52515523b5335731455beb738a24
nyfine99 <noahyfine@gmail.com>
2026-04-21 17:06:23 -0400

    Added max line length to output log files for cleaner viewing

--- data/make_log_files.py
+++ data/make_log_files.py
@@ -1,11 +1,20 @@
 import bson
 import os
+import textwrap
 from datetime import datetime
+
+
+# SETTINGS
 
 USER_IDS = ['657fa05267050fd8b627f0bc', '662fb80a4da196677a2d2a16', '6644b750f83b1ad48d9ce14f', '6715b46f9f59f9e05837029a', '67e268cbf13dcd115337894c', '682a1996cd2cfa1f1160cb71', '683cbc0c0c2d722831c8bc17', '699397c4953cfe8ddd54a8f0', '69c2d2d68e61037f1e2f1a06', '69d7ebaa0c636510b15ce777']
   # change this to whichever rooms you want
 
+MAX_LINE_LENGTH = 80  # max line length; NONE for no line length
+
 OUTPUT_DIR = "data/experiment_results/Frozone_Data/chatlogs"
+
+
+# SCRIPT
 
 with open("data/experiment_results/Frozone_Data/chatlogs/chatrooms_4-14.bson", "rb") as f:
     data = bson.decode_all(f.read())
@@ -40,7 +49,11 @@ for doc in data:
         sender = msg.get('sender', '?')
         sender_true_id = true_id_map[sender]
         text = msg.get('message', '')
-        lines.append(f"  [{ts}] {sender} ({sender_true_id}): {text}")
+        line = f"  [{ts}] {sender} ({sender_true_id}): {text}"
+        if MAX_LINE_LENGTH:
+            prefix = " " * 4
+            line = textwrap.fill(line, width=MAX_LINE_LENGTH, subsequent_indent=prefix)
+        lines.append(line)
 
     filename = os.path.join(OUTPUT_DIR, f"chatlog_{doc['user_id']}.txt")
     with open(filename, "w", encoding="utf-8") as f:
